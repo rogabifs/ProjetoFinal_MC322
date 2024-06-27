953ec804c61eb9c7f563b4275c193dc24241adb8
samineses <s188132@dac.unicamp.br>
2024-06-27 17:14:34 -0300

    edições finais

--- ProjetoFinalMC322/ProjetoFinalMC322/Model/HotelModel.cs
+++ ProjetoFinalMC322/ProjetoFinalMC322/Model/HotelModel.cs
@@ -4,74 +4,73 @@ using ProjetoFinalMC322.ViewModels;
 
 namespace ProjetoFinalMC322.Model;
 
-public class HotelModel
-{
-   private List<ClientesModel> clientes;
-        private List<QuartosModel> quartos;
-        private List<FuncionariosModel> funcionarios;
-        private RestauranteModel restaurante;
+public class HotelModel{
+    private List<ClientesModel> clientes;
+    private List<QuartosModel> quartos;
+    private List<FuncionariosModel> funcionarios;
+    private RestauranteModel restaurante;
 
-        public HotelModel()
-        {
-            clientes = new List<ClientesModel>();
-            quartos = new List<QuartosModel>();
-            funcionarios = new List<FuncionariosModel>();
-            restaurante = new RestauranteModel("hello");
-        }
+    public HotelModel(){
+            
+        clientes = new List<ClientesModel>();
+        quartos = new List<QuartosModel>();
+        funcionarios = new List<FuncionariosModel>();
+        restaurante = new RestauranteModel("hello");
+    }
 
-        public List<ClientesModel> Clientes => clientes;
-        public List<QuartosModel> Quartos => quartos;
-        public List<FuncionariosModel> Funcionarios => funcionarios;
-        public RestauranteModel Restaurante => restaurante;
+    public List<ClientesModel> Clientes => clientes;
+    public List<QuartosModel> Quartos => quartos;
+    public List<FuncionariosModel> Funcionarios => funcionarios;
+    public RestauranteModel Restaurante => restaurante;
 
-        //getters and setters
-        public List<ClientesModel> GetClientes()
-        {
-            return clientes;
-        }
+    //getters and setters
+    public List<ClientesModel> GetClientes()
+    {
+        return clientes;
+    }
 
-        public List<QuartosModel> GetQuartos()
-        {
-            return quartos;
-        }
+    public List<QuartosModel> GetQuartos()
+    {
+        return quartos;
+    }
 
-        public List<FuncionariosModel> GetFuncionarios()
-        {
-            return funcionarios;
-        }
+    public List<FuncionariosModel> GetFuncionarios()
+    {
+        return funcionarios;
+    }
 
-        public RestauranteModel GetRestaurante()
-        {
-            return restaurante;
-        }
+    public RestauranteModel GetRestaurante()
+    {
+        return restaurante;
+    }
 
-        public void SetClientes(List<ClientesModel> clientes)
-        {
-            this.clientes = clientes;
-        }
+    public void SetClientes(List<ClientesModel> clientes)
+    {
+        this.clientes = clientes;
+    }
 
-        public void SetQuartos(List<QuartosModel> quartos)
-        {
-            this.quartos = quartos;
-        }
+    public void SetQuartos(List<QuartosModel> quartos)
+    {
+        this.quartos = quartos;
+    }
 
 
-        // Add methods for adding clients, rooms, employees, etc. (e.g., AdicionarCliente, AdicionarFuncionario)
-        public void AdicionarQuarto(QuartosModel quarto)
-        {
-            quartos.Add(quarto);
-        }
+    //METODO ADICIONAR QUARTO
+    public void AdicionarQuarto(QuartosModel quarto)
+    {
+        quartos.Add(quarto);
+    }
 
-        public List<QuartosModel> QuartosDisponiveis()
-        {
-            List<QuartosModel> disponiveis = new List<QuartosModel>();
-            foreach (QuartosModel quarto in quartos)
-            {
-                if (quarto.GetDisponivel())
-                {
-                    disponiveis.Add(quarto);
-                }
+    //METODO LISTAR QUARTOS DISPONIVEIS
+    public List<QuartosModel> QuartosDisponiveis(){
+        
+        List<QuartosModel> disponiveis = new List<QuartosModel>();
+        foreach (QuartosModel quarto in quartos){
+            
+            if (quarto.GetDisponivel()){
+                disponiveis.Add(quarto);
             }
-            return disponiveis;
         }
+        return disponiveis;
+    }
 }
